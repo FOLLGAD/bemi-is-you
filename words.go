678ac327e139fc4fbf62219ed60f3360f8395c3a
Emil Ahlbäck <emil.ahlback99@gmail.com>
2019-05-09 14:38:32 +0200

    Word functions now take pointers

--- words.go
+++ words.go
@@ -9,70 +9,20 @@ type Meaning struct {
 	modifier []string // The adjectives
 }
 
-func wording(words []Object) []Meaning {
-	if len(words) < 3 {
-		// Too short
-		return []Meaning{}
-	}
-
-	currently := false
-	currentHead := 0
-	hasEncounteredIs := false
-
-	// Check sentence validity
-	for i := 0; i < len(words); i++ {
-		word := words[i]
-
-		shouldBeConj := (i-currentHead)%2 == 1
-
-		if word.Kind == Conj {
-			if currently == false {
-				continue
-			}
-			if !shouldBeConj {
-				parseSentence(words[currentHead : i-1])
-				currently = false
-			} else if word.Item == "and" {
-				continue
-			} else if hasEncounteredIs {
-				parseSentence(words[currentHead : i-1])
-				currently = false
-			}
-			if word.Item == "is" {
-				hasEncounteredIs = true
-			}
-			continue // Should be conjuction, and is.
-		} else if currently {
-			if shouldBeConj {
-				parseSentence(words[currentHead : i-1])
-				currently = false
-			} else if word.Kind == Noun {
-				continue
-			}
-		} else if word.Kind == Noun {
-			currentHead = i
-			currently = true
-		} else if word.Kind == Adj {
-
-		}
-	}
-	return []Meaning{}
-}
-
-func newWording(words []Object) []Meaning {
-	isWords := []Object{}
+func findSentences(words ObjectList) []Meaning {
+	isWords := ObjectList{}
 	for _, e := range words {
 		if e.Kind == Conj && e.Item == "is" {
 			isWords = append(isWords, e)
 		}
 	}
-	findWordByPos := func(pos Pos) (Object, error) {
+	findWordByPos := func(pos Pos) (*Object, error) {
 		for _, e := range words {
 			if e.Pos == pos {
 				return e, nil
 			}
 		}
-		return Object{}, errors.New("Not found")
+		return &Object{}, errors.New("Not found")
 	}
 	meanings := []Meaning{}
 
@@ -90,7 +40,7 @@ func newWording(words []Object) []Meaning {
 			objForw, errForw := findWordByPos(posForw)
 			objBack, errBack := findWordByPos(posBack)
 			if errForw == nil && errBack == nil && objBack.Kind == Noun && (objForw.Kind == Noun || objForw.Kind == Adj) {
-				m := parseSentence([]Object{objBack, e, objForw})
+				m := parseSentence(ObjectList{objBack, e, objForw})
 				meanings = append(meanings, m)
 			}
 		}
@@ -108,7 +58,7 @@ func newWording(words []Object) []Meaning {
 			objForw, errForw := findWordByPos(posForw)
 			objBack, errBack := findWordByPos(posBack)
 			if errForw == nil && errBack == nil && objBack.Kind == Noun && (objForw.Kind == Noun || objForw.Kind == Adj) {
-				m := parseSentence([]Object{objBack, e, objForw})
+				m := parseSentence(ObjectList{objBack, e, objForw})
 				meanings = append(meanings, m)
 			}
 		}
@@ -117,7 +67,7 @@ func newWording(words []Object) []Meaning {
 }
 
 // Parse a correct sentence
-func parseSentence(words []Object) Meaning {
+func parseSentence(words ObjectList) Meaning {
 	affected := []string{}
 	modifier := []string{}
 
