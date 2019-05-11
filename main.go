52bf0e31b35e48f05fc67fefdde38cb649506e72
Emil Ahlbäck <emil.ahlback99@gmail.com>
2019-05-11 16:46:28 +0200

    Updated level, fixed server player numbering

--- main.go
+++ main.go
@@ -29,13 +29,37 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+var idCounter Id = 0
+
+func addObject(pos Pos, kind Kind, item string) *Object {
+	obj := &Object{
+		pos,
+		kind,
+		item,
+		idCounter,
+	}
+	idCounter++
+	return obj
+}
+
 func main() {
 	// LEVEL
 	var firstLevel = Level{}
 	firstLevel.Height = 20
 	firstLevel.Width = 25
-	var bemi = &Object{Pos{10, 10}, Char, "bemi", 0}
-	firstLevel.Objects = []*Object{bemi}
+
+	firstLevel.Objects = []*Object{
+		addObject(Pos{10, 10}, Char, "bemi"),
+		addObject(Pos{8, 10}, Char, "sami"),
+		addObject(Pos{0, 0}, Noun, "bemi"),
+		addObject(Pos{1, 0}, Conj, "is"),
+		addObject(Pos{2, 0}, Adj, "1"),
+		addObject(Pos{0, 5}, Noun, "sami"),
+		addObject(Pos{1, 5}, Conj, "is"),
+		addObject(Pos{2, 5}, Adj, "1"),
+		addObject(Pos{0, 6}, Conj, "is"),
+		addObject(Pos{0, 7}, Adj, "2"),
+	}
 	// ******
 
 	players := []Player{}
@@ -46,9 +70,11 @@ func main() {
 		// Read incoming tick updates and them broadcast to all players
 		for {
 			tick := <-updateChan
-			js, _ := json.Marshal(Message{tick, 1})
-			for _, p := range players {
-				p.conn.WriteJSON(js)
+			if len(tick) > 0 {
+				js, _ := json.Marshal(Message{tick, 1})
+				for _, p := range players {
+					p.conn.WriteJSON(js)
+				}
 			}
 		}
 	}()
@@ -62,10 +88,10 @@ func main() {
 		conn, _ := upgrader.Upgrade(w, r, nil)
 		js, _ := json.Marshal(Message{firstLevel, 0})
 
-		playerPos := len(players)
-		newPlayer := Player{playerPos, conn}
+		playerNum := len(players) + 1
+		newPlayer := Player{playerNum, conn}
 		players = append(players, newPlayer)
-		fmt.Println("New player ", newPlayer.number)
+		fmt.Println("New player", newPlayer.number)
 
 		conn.WriteJSON(js)
 		for {
@@ -75,9 +101,12 @@ func main() {
 				fmt.Println(err)
 				conn.Close()
 
-				players = append(players[:playerPos], players[playerPos+1:]...)
-
-				return
+				for i := range players {
+					if players[i].number == playerNum {
+						players = append(players[:i], players[i+1:]...)
+						return
+					}
+				}
 			}
 
 			var message ReceivedMessage
