010866aa8b94c28dc7aece4e725a329db3d7d3c9
Samuel Söderberg <sasoder@kth.se>
2019-05-16 15:02:12 +0200

    Added even more epic levels

--- levels.go
+++ levels.go
@@ -338,13 +338,11 @@ func getLevel(level int) Level {
 				addObject(Pos{2, 7}, Char, "wall"),
 				addObject(Pos{7, 7}, Char, "wall"),
 				addObject(Pos{15, 7}, Char, "wall"),
-				addObject(Pos{16, 7}, Char, "wall"),
 
 				addObject(Pos{2, 8}, Char, "wall"),
 				addObject(Pos{7, 8}, Char, "wall"),
-				addObject(Pos{16, 8}, Char, "wall"),
+				addObject(Pos{15, 8}, Char, "wall"),
 				addObject(Pos{15, 9}, Char, "wall"),
-				addObject(Pos{16, 9}, Char, "wall"),
 				addObject(Pos{2, 9}, Char, "wall"),
 				addObject(Pos{7, 9}, Char, "wall"),
 
@@ -385,6 +383,232 @@ func getLevel(level int) Level {
 				addObject(Pos{6, 6}, Conj, "is"),
 			},
 		}
+	} else if level == 8 {
+		return Level{
+			Width:  18,
+			Height: 15,
+			Number: 6,
+			Objects: []Object{
+
+				addObject(Pos{11, 8}, Adj, "push"),
+				addObject(Pos{8, 5}, Adj, "win"),
+
+				addObject(Pos{10, 1}, Noun, "star"),
+				addObject(Pos{11, 1}, Conj, "is"),
+				addObject(Pos{12, 1}, Adj, "defeat"),
+
+				addObject(Pos{0, 1}, Noun, "wall"),
+				addObject(Pos{4, 5}, Noun, "wall"),
+				addObject(Pos{1, 1}, Conj, "is"),
+				addObject(Pos{2, 1}, Adj, "stop"),
+				addObject(Pos{5, 5}, Conj, "is"),
+
+				addObject(Pos{5, 1}, Noun, "bemi"),
+				addObject(Pos{6, 1}, Conj, "is"),
+				addObject(Pos{7, 1}, Adj, "1"),
+				addObject(Pos{5, 6}, Char, "bemi"),
+
+				addObject(Pos{2, 4}, Char, "wall"),
+				addObject(Pos{3, 4}, Char, "wall"),
+				addObject(Pos{4, 4}, Char, "wall"),
+				addObject(Pos{5, 4}, Char, "wall"),
+				addObject(Pos{6, 4}, Char, "wall"),
+				addObject(Pos{7, 4}, Char, "wall"),
+
+				addObject(Pos{2, 5}, Char, "wall"),
+				addObject(Pos{7, 5}, Char, "wall"),
+
+				addObject(Pos{2, 6}, Char, "wall"),
+				addObject(Pos{7, 6}, Char, "wall"),
+				addObject(Pos{8, 6}, Char, "wall"),
+				addObject(Pos{9, 6}, Char, "wall"),
+				addObject(Pos{10, 6}, Char, "wall"),
+				addObject(Pos{11, 6}, Char, "wall"),
+				addObject(Pos{12, 6}, Char, "wall"),
+				addObject(Pos{13, 6}, Char, "wall"),
+				addObject(Pos{14, 6}, Char, "wall"),
+				addObject(Pos{15, 6}, Char, "wall"),
+
+				addObject(Pos{2, 7}, Char, "wall"),
+				addObject(Pos{7, 7}, Char, "wall"),
+				addObject(Pos{15, 7}, Char, "wall"),
+
+				addObject(Pos{2, 8}, Char, "wall"),
+				addObject(Pos{7, 8}, Char, "wall"),
+				addObject(Pos{15, 8}, Char, "wall"),
+				addObject(Pos{15, 9}, Char, "wall"),
+				addObject(Pos{2, 9}, Char, "wall"),
+				addObject(Pos{7, 9}, Char, "wall"),
+
+				addObject(Pos{15, 10}, Char, "wall"),
+				addObject(Pos{2, 10}, Char, "wall"),
+				addObject(Pos{7, 10}, Char, "wall"),
+
+				addObject(Pos{15, 11}, Char, "wall"),
+				addObject(Pos{2, 11}, Char, "wall"),
+				addObject(Pos{7, 11}, Char, "wall"),
+
+				addObject(Pos{15, 12}, Char, "wall"),
+				addObject(Pos{2, 12}, Char, "wall"),
+				addObject(Pos{7, 12}, Char, "star"),
+
+				addObject(Pos{2, 13}, Char, "wall"),
+				addObject(Pos{2, 14}, Char, "wall"),
+				addObject(Pos{3, 14}, Char, "wall"),
+				addObject(Pos{4, 14}, Char, "wall"),
+				addObject(Pos{5, 14}, Char, "wall"),
+				addObject(Pos{6, 14}, Char, "wall"),
+				addObject(Pos{7, 14}, Char, "wall"),
+				addObject(Pos{7, 13}, Char, "wall"),
+				addObject(Pos{8, 13}, Char, "wall"),
+				addObject(Pos{9, 13}, Char, "wall"),
+				addObject(Pos{10, 13}, Char, "wall"),
+				addObject(Pos{11, 13}, Char, "wall"),
+				addObject(Pos{12, 13}, Char, "wall"),
+				addObject(Pos{13, 13}, Char, "wall"),
+				addObject(Pos{14, 13}, Char, "wall"),
+				addObject(Pos{15, 13}, Char, "wall"),
+
+				addObject(Pos{14, 8}, Noun, "sami"),
+				addObject(Pos{14, 9}, Conj, "is"),
+				addObject(Pos{14, 10}, Adj, "2"),
+				addObject(Pos{12, 9}, Char, "sami"),
+			},
+		}
+	} else if level == 9 {
+		return Level{
+			Width:  18,
+			Height: 15,
+			Number: 6,
+			Objects: []Object{
+
+				addObject(Pos{11, 3}, Adj, "push"),
+				addObject(Pos{11, 4}, Conj, "is"),
+				addObject(Pos{8, 14}, Adj, "win"),
+
+				addObject(Pos{0, 2}, Char, "fish"),
+
+				addObject(Pos{10, 1}, Noun, "star"),
+				addObject(Pos{11, 1}, Conj, "is"),
+				addObject(Pos{12, 1}, Adj, "defeat"),
+
+				addObject(Pos{0, 1}, Noun, "wall"),
+				addObject(Pos{4, 7}, Noun, "fish"),
+				addObject(Pos{1, 1}, Conj, "is"),
+				addObject(Pos{2, 1}, Adj, "stop"),
+
+				addObject(Pos{5, 1}, Noun, "bemi"),
+				addObject(Pos{6, 1}, Conj, "is"),
+				addObject(Pos{7, 1}, Adj, "1"),
+				addObject(Pos{5, 6}, Char, "bemi"),
+
+				addObject(Pos{0, 0}, Char, "wall"),
+				addObject(Pos{1, 0}, Char, "wall"),
+				addObject(Pos{2, 0}, Char, "wall"),
+				addObject(Pos{3, 0}, Char, "wall"),
+				addObject(Pos{4, 0}, Char, "wall"),
+				addObject(Pos{5, 0}, Char, "wall"),
+				addObject(Pos{6, 0}, Char, "wall"),
+				addObject(Pos{7, 0}, Char, "wall"),
+				addObject(Pos{8, 0}, Char, "wall"),
+				addObject(Pos{9, 0}, Char, "wall"),
+				addObject(Pos{10, 0}, Char, "wall"),
+				addObject(Pos{11, 0}, Char, "wall"),
+				addObject(Pos{12, 0}, Char, "wall"),
+				addObject(Pos{13, 0}, Char, "wall"),
+				addObject(Pos{13, 1}, Char, "wall"),
+
+				addObject(Pos{2, 4}, Char, "wall"),
+				addObject(Pos{3, 4}, Char, "wall"),
+				addObject(Pos{4, 4}, Char, "wall"),
+				addObject(Pos{5, 4}, Char, "wall"),
+				addObject(Pos{6, 4}, Char, "wall"),
+				addObject(Pos{7, 4}, Char, "wall"),
+
+				addObject(Pos{2, 5}, Char, "wall"),
+				addObject(Pos{7, 5}, Char, "wall"),
+
+				addObject(Pos{2, 6}, Char, "wall"),
+				addObject(Pos{7, 6}, Char, "wall"),
+				addObject(Pos{8, 6}, Char, "wall"),
+				addObject(Pos{9, 6}, Char, "wall"),
+				addObject(Pos{10, 6}, Char, "wall"),
+				addObject(Pos{11, 6}, Char, "wall"),
+				addObject(Pos{12, 6}, Char, "wall"),
+				addObject(Pos{13, 6}, Char, "wall"),
+				addObject(Pos{14, 6}, Char, "wall"),
+				addObject(Pos{15, 6}, Char, "wall"),
+
+				addObject(Pos{2, 7}, Char, "wall"),
+				addObject(Pos{7, 7}, Char, "wall"),
+				addObject(Pos{15, 7}, Char, "wall"),
+
+				addObject(Pos{2, 8}, Char, "wall"),
+				addObject(Pos{7, 8}, Char, "wall"),
+				addObject(Pos{15, 8}, Char, "wall"),
+				addObject(Pos{15, 9}, Char, "wall"),
+				addObject(Pos{2, 9}, Char, "wall"),
+				addObject(Pos{7, 9}, Char, "wall"),
+
+				addObject(Pos{15, 10}, Char, "wall"),
+				addObject(Pos{2, 10}, Char, "wall"),
+				addObject(Pos{7, 10}, Char, "wall"),
+
+				addObject(Pos{15, 11}, Char, "wall"),
+				addObject(Pos{2, 11}, Char, "wall"),
+				addObject(Pos{7, 11}, Char, "wall"),
+
+				addObject(Pos{15, 12}, Char, "wall"),
+				addObject(Pos{2, 12}, Char, "wall"),
+				addObject(Pos{7, 12}, Char, "star"),
+
+				addObject(Pos{2, 13}, Char, "wall"),
+				addObject(Pos{3, 13}, Char, "wall"),
+				addObject(Pos{5, 13}, Char, "wall"),
+				addObject(Pos{6, 13}, Char, "wall"),
+				addObject(Pos{3, 14}, Char, "wall"),
+				addObject(Pos{4, 14}, Char, "wall"),
+				addObject(Pos{5, 14}, Char, "wall"),
+				addObject(Pos{7, 13}, Char, "wall"),
+				addObject(Pos{8, 13}, Char, "wall"),
+				addObject(Pos{9, 13}, Char, "wall"),
+				addObject(Pos{10, 13}, Char, "wall"),
+				addObject(Pos{11, 13}, Char, "wall"),
+				addObject(Pos{12, 13}, Char, "wall"),
+				addObject(Pos{13, 13}, Char, "wall"),
+				addObject(Pos{14, 13}, Char, "wall"),
+				addObject(Pos{15, 13}, Char, "wall"),
+
+				addObject(Pos{11, 12}, Noun, "sami"),
+				addObject(Pos{12, 12}, Conj, "is"),
+				addObject(Pos{13, 12}, Adj, "2"),
+				addObject(Pos{12, 9}, Char, "sami"),
+			},
+		}
+	}
+
+	//default level for testing and what not
+	return Level{
+		Width:  1,
+		Height: 1,
+		Objects: []Object{
+			addObject(Pos{2, 10}, Char, "bemi"),
+			addObject(Pos{16, 10}, Char, "sami"),
+
+			addObject(Pos{9, 5}, Char, "wall"),
+
+			addObject(Pos{0, 0}, Noun, "star"),
+			addObject(Pos{1, 0}, Conj, "is"),
+			addObject(Pos{2, 0}, Adj, "defeat"),
+
+			addObject(Pos{0, 2}, Noun, "bemi"),
+			addObject(Pos{1, 2}, Conj, "is"),
+			addObject(Pos{2, 2}, Adj, "1"),
+
+			addObject(Pos{0, 5}, Noun, "sami"),
+			addObject(Pos{1, 5}, Conj, "is"),
+			addObject(Pos{3, 5}, Adj, "2"),
+			addObject(Pos{11, 1}, Char, "star")},
 	}
 
 	//default level for testing and what not
